6a600efd3b8b4011f90990400cfe252d64a1ef28
Petar Ristic <petrarkar@proton.me>
2025-02-09 18:37:23 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 #!bin/bash
 
-sudo chmod +x eth.sh
+sudo chmod +x eth1.sh
 sudo chmod +x cam.sh
 sudo chmod +x rpi-connect.sh
 sudo chmod +x hailo.sh
 
-sudo ./ eth.sh
+sudo ./ eth1.sh
 sudo ./cam.sh
 sudo ./rpi-connect.sh
 sudo ./hailo.sh
