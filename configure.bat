bfc4bffc62b1fb9637137add4f3fb3b6231de479
Petar Ristic <petrarkar@proton.me>
2025-12-28 16:03:54 +0100

    Simplify configuration by setting P value to 0
    Removed prompts for P value and defaulted it to 0.

--- configure.bat
+++ configure.bat
@@ -2,25 +2,15 @@
 setlocal ENABLEEXTENSIONS ENABLEDELAYEDEXPANSION
 echo === Raspberry Pi Config Generator ===
 echo.
-
 set /p I_VALUE=Enter I value (stavite 00001, 00002, 00003...): 
 if "%I_VALUE%"=="" (
     echo Error: I ne moze biti prazan
     goto error
 )
 
-set /p P_VALUE=Enter P value (stavite 0 za pocetak od nule): 
-if "%P_VALUE%"=="" set P_VALUE=0
-
-echo %P_VALUE% | findstr /R "^[0-9][0-9]*$" >nul
-if errorlevel 1 (
-    echo Error: P mora biti validan celi broj
-    goto error
-)
-
 (
     echo I:%I_VALUE%
-    echo P:%P_VALUE%
+    echo P:0
 ) > cnf.txt
 
 echo.
